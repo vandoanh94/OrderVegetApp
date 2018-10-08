c31d77c06cbbae93a24effef2818d66560bd07a5
ORIENTSOFTWARE\nvdoanh <vandoanh94@gmail.com>
2018-10-08 13:37:47 +0700

    config and add data to database firebase, ok done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { firebaseConfig } from './credentials';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBJ8mzzk1ujuH_No0yWu3BqrJNkc7aO1bI",
+    authDomain: "ordervegetapp.firebaseapp.com",
+    databaseURL: "https://ordervegetapp.firebaseio.com",
+    projectId: "ordervegetapp",
+    storageBucket: "ordervegetapp.appspot.com",
+    messagingSenderId: "92761907523"
+  };
